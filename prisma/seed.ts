b72b93671d459296577b24b3cefb7b16a37eee48
Abdessamad Abdaoui <abdssamadabdaoui@gmail.com>
2025-06-01 23:46:02 +0100

    adding the emplois manupilation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -48,7 +48,7 @@
 // haha i have problem with the ts extension.
 
 import prisma from "@/lib/prisma";
-import { Classe, Filiere , Etudiant } from "@prisma/client";
+import { Classe, Filiere, Etudiant, Day, NumeroSeance, Seance } from "@prisma/client";
 
 
 
@@ -93,24 +93,59 @@ import { Classe, Filiere , Etudiant } from "@prisma/client";
 
 async function main() {
 
-    // const Etudiant: (Etudiant & {classe : Classe}) | null;
-  const etudiant = await prisma.etudiant.findUnique({
-        where: { emailEtd:'dfaffa@gmil.com'  },
-        include: {
-            classe:{
-            select:{
-                idClasse:true,
-                semestre: true
-            }
-            }
-
+    const seances = await prisma.seance.findMany({
+        where: {
+            idClasse: 200
         }
+
     })
 
-    console.log('etudiant?.classe.semestre= '+etudiant?.classe.semestre);
+    function sessionExists(day: Day, num: number): boolean {
+        let order: NumeroSeance = getCorrespondingOrder(num);
+        console.log("order=", order);
+        console.log(`seances[0].day =`, seances[0].day);
+        console.log(`seances[0].numeroSeance=`, seances[0].numeroSeance);
+        for ( let seance of seances ) {
+             console.log(`seance.day === day = `, seance.day === day && seance.numeroSeance === order);
+            if (seance.day === day && seance.numeroSeance === order) return true;
+        }
+        return false;
+        }
+        // seances.some((seance, index) => {
+        //     // console.log(`${index} seances[0].day =` ,seance.day);
+        //     // console.log(`${index}  seances[0].numeroSeance=`, seance.numeroSeance);
+        //     console.log(`seance.day === day = `, seance.day === day && seance.numeroSeance === order);
+        //     if (seance.day === day && seance.numeroSeance === order) return true;
+        // });
+        // return false;
+    
+
+    function getCorrespondingOrder(num: number): NumeroSeance {
+        switch (num) {
+            case 1: return 'FIRST'
+            case 2: return 'SECOND'
+            case 3: return 'THIRD'
+            case 4: return 'FOURTH'
+            case 5: return 'FIFTH'
+            case 6: return 'SIXTH'
+            default: return 'FIRST'
+        }
+    }
+    const day: Day = 'TUESDAY'
+
+     function getSession(day : Day, num : number): Seance|null {
+        let order:NumeroSeance = getCorrespondingOrder(num);
+        for (let seance of seances ) {
+            if( seance.day === day && seance.numeroSeance === order ) return seance;
+        }
+        return null;
+     };
+
+       console.log("sessionExists('MONDAY',i)= ", sessionExists('MONDAY',1) );
+        
+
+
 }
-
-
 
 
 
